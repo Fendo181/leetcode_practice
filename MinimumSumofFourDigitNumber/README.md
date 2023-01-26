ab24a4c2cc320671f918be670815048d112799ca
Fendo181 <fendo181git@gmail.com>
2023-01-26 14:25:05 +0900

    正しい解答法を追記した

--- MinimumSumofFourDigitNumber/README.md
+++ MinimumSumofFourDigitNumber/README.md
@@ -1,5 +1,5 @@
 [Minimum Sum of Four Digit Number After Splitting Digits - LeetCode](https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/)
 
-- [カプレカ数](https://dianxnao.com/c%EF%BC%9A4%E6%A1%81%E3%81%AE%E3%82%AB%E3%83%97%E3%83%AC%E3%82%AB%E6%95%B0%E3%82%92%E5%86%8D%E7%8F%BE%E3%81%97%E3%81%A6%E3%81%BF%E3%82%8B/)のアルゴリズムで試したけどうまくいかなかった。
+- ~~[カプレカ数](https://dianxnao.com/c%EF%BC%9A4%E6%A1%81%E3%81%AE%E3%82%AB%E3%83%97%E3%83%AC%E3%82%AB%E6%95%B0%E3%82%92%E5%86%8D%E7%8F%BE%E3%81%97%E3%81%A6%E3%81%BF%E3%82%8B/)のアルゴリズムで試したけどうまくいかなかった。~~
 
-本当はもっとわかりやすいアルゴリズムがありそう....
+ノートに数式を書いてみると、ソート後にに `1番目×10+4番目, 2番目×10+3番目` の組み合わせが最小になる。
--- MinimumSumofFourDigitNumber/ans.php
+++ MinimumSumofFourDigitNumber/ans.php
@@ -9,19 +9,9 @@ require_once '../vendor/autoload.php';
  * @return Integer
  */
 function minimumSum($num) {
-    while(true){
-        $numSort = str_split($num);
-        ($numSort);
-        $numRsort = str_split($num);
-        rsort($numRsort);
-        $num = (int)(implode($numRsort) - implode($numSort));
-        $tmpNum = $num;
-
-        if($tmpNum == $num) break;
-    }
-    $result = str_split($tmpNum);
-    sort($result);
-    return (int)($result[0].$result[1]) + (int)($result[2].$result[3]);
+    $numSort = str_split($num);
+    sort($numSort);
+    return (($numSort[0]*10) + $numSort[2]) + (($numSort[1]*10) + $numSort[3]);
 }
 
 var_dump(minimumSum(2932));
